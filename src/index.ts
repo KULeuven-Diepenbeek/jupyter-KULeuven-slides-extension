4795e389cc2c0a46b440407902ea159e1b063bf0
Jan Genoe <jan.genoe@gmail.com>
2024-05-07 23:33:26 +0200

    src update

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
  * Initialization data for the jupyter-KULeuven-slides-extension extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyter-KULeuven-slides-extension:plugin',
+  id: 'jupyter-KULeuven-slides-extension:slides',
   description: 'A quick way to enter slide titles for the jupyter_KULeuven Reveal.js slides',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
