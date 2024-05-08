d78da9d6b300836e757dbe145d56cf3288323251
Jan Genoe <jan.genoe@gmail.com>
2024-05-09 01:41:40 +0200

    linter update

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,13 @@ import {
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyter-KULeuven-slides-extension:slides',
-  description: 'A quick way to enter slide titles for the jupyter_KULeuven Reveal.js slides',
+  description:
+    'A quick way to enter slide titles for the jupyter_KULeuven Reveal.js slides',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyter-KULeuven-slides-extension is activated!');
+    console.log(
+      'JupyterLab extension jupyter-KULeuven-slides-extension is activated!'
+    );
   }
 };
 
